6c890d1e360121f62bdf03f0f6686b644c955628
davidkelley <david.james.kelley@gmail.com>
2014-02-27 21:07:57 +0000

    modified parameters spec tests

--- spec/launcher/parameters_spec.rb
+++ spec/launcher/parameters_spec.rb
@@ -4,9 +4,12 @@ require 'launcher/parameters'
 
 describe Launcher::Parameters do
 
+  let(:test_values) { { :foo => "bar", :x => "y" } }
+
   before do
     Launcher::Config::AWS.stub(:configured?) { false }
     @parameters = build(:parameters)
+    @parameters_with_values = Launcher::Parameters.new(test_values)
   end
 
   subject { @parameters }
@@ -19,11 +22,7 @@ describe Launcher::Parameters do
   it { should respond_to(:select) }
 
   describe "return value of #all" do
-
-    let(:test_values) { { :foo => "bar", :x => "y" } }
     
-    before { @parameters_with_values = Launcher::Parameters.new(test_values) }
-
     it "should be a hash" do
       expect(@parameters_with_values.all).to be_a(Hash)
     end
@@ -31,6 +30,38 @@ describe Launcher::Parameters do
     it "should return the initialized values" do
       expect(@parameters_with_values.all).to eq(test_values)
     end
+  end
+
+  describe "return value of #[](key)" do
+
+    describe "when a key exists" do
+      it "should return the value" do
+        expect(@parameters_with_values[:foo]).to eq("bar")
+      end
+    end
+
+    describe "when a key does not exist" do
+      it "should return the value" do
+        expect(@parameters_with_values[:boo]).to be_nil
+      end
+    end
+
+  end
+
+  describe "return value of #select" do
+
+    it "should be a hash" do
+      expect(@parameters_with_values.select(:foo, :bar)).to be_a(Hash)
+    end
+
+    it "should match the keys defined" do
+      expect(@parameters_with_values.select(*test_values.keys).keys).to eq test_values.keys
+    end
+
+    it "should return a hash of the keys and values defined" do
+      expect(@parameters_with_values.select(*test_values.keys)).to eq test_values
+    end
+
   end
   
 end
