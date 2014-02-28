91e1db8fb6e11d054af169580729fa9ab571a3ee
vagrant <vagrant@precise64.(none)>
2014-02-28 16:25:52 +0000

    Added log spec tests

--- spec/launcher/log_spec.rb
+++ spec/launcher/log_spec.rb
@@ -17,6 +17,28 @@ describe Launcher::Log do
     let(:msg) { "This is a test message" }
     let(:time) { Time.now }
 
+    it "should return a correctly formatted message" do
+      message = Launcher::Log.format_msg(severity, msg, time)
+      expected_message = "[#{time}] [#{severity}]: #{msg}\n"
+      expect(message).to eq(expected_message)
+    end
+
+  end
+
+  describe "action of #color" do
+
+    let(:message) { "a message" }
+    let(:type) { :fatal }
+
+    it "should retrieve the correct color for a message" do
+      Launcher::Log.should_receive(:color_fatal).with(message)
+      Launcher::Log.color(message, type)
+    end
+
+    it "should correctly stylize a message" do
+      expect(Launcher::Log.color(message,type)).to eq(message.underline.red)
+    end
+    
   end
   
 end
